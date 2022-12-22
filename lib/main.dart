dad9fc2e8c40d8c324ac8e6fb3728ce6dec08279
Aditya Anand <aditechdevofficial@gmail.com>
2022-12-23 03:05:04 +0530

    :tada: Basic register screen UI done

--- lib/main.dart
+++ lib/main.dart
@@ -7,31 +7,80 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
-    ),
-  );
+    );
+  }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _userName;
+  late final TextEditingController _pwd;
+
+  @override
+  void initState() {
+    super.initState();
+    _userName = TextEditingController();
+    _pwd = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _userName.dispose();
+    _pwd.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register"),
+      ),
       body: Center(
-        child: Text(
-          '<  START CODING / >',
-          style: TextStyle(
-            fontSize: 20,
-            color: Colors.blue,
-          ),
-          textAlign: TextAlign.center,
+        child: Column(
+          children: [
+            TextField(
+              controller: _userName,
+              decoration: const InputDecoration(
+                hintText: "Email",
+              ),
+            ),
+            TextField(
+              controller: _pwd,
+              decoration: const InputDecoration(
+                hintText: "Password",
+              ),
+              // autofillHints: "Email",
+            ),
+            TextButton(
+              child: const Text(
+                "Register",
+              ),
+              onPressed: () {
+                print("Register");
+              },
+            ),
+          ],
         ),
       ),
     );
