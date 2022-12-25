bf2b9b73f3cd3c0f7868a3ad4d2156453434480c
Aditya Anand <aditechdevofficial@gmail.com>
2022-12-26 03:50:12 +0530

    :sparkles: Exceptions and Auth User

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,16 @@
+// login exceptions
+class UserNotFoundAuthExceptions implements Exception {}
+
+class WrongPasswordAuthExceptions implements Exception {}
+
+// register
+class WeakPasswordAuthExceptions implements Exception {}
+
+class EmailAlreadyInUseExceptions implements Exception {}
+
+class InvalidAuthExceptions implements Exception {}
+
+// generic
+class GenericAuthExceptions implements Exception {}
+
+class UserNotLoggedInAuthExceptions implements Exception {}
